6495445b5415fc5c46cfae09bc594f969180f4cd
Hossam Hassan <hossam.sahss120@gmail.com>
2023-09-23 05:41:03 +0300

    Auto Commit

--- commands.c
+++ commands.c
@@ -14,41 +14,51 @@
  ***/
 void analyze(char **arguments, general_t *info, char *buff)
 {
-	char *cmd = arguments[0];
-	int status = is_file(cmd);
-	char *value_path = which(cmd, info);
+    char *cmd;
+    int status;
 
-	if (arguments == NULL || *arguments == NULL)
-		return;
+    if (*arguments == NULL || arguments == NULL)
+        return;
 
-	info->command = cmd;
+    cmd = arguments[0];
+    info->command = cmd;
+    if (check_builtin(info, arguments) == _TRUE)
+        return;
 
-	if (check_builtin(info, arguments) == _TRUE)
-		return;
+    status = is_file(cmd);
+    if (status == NON_PERMISSIONS)
+    {
+        info->status_code = 126;
+        info->error_code = _CODE_EACCES;
+        error(info);
+        return;
+    }
 
-	if (status == NON_PERMISSIONS)
-	{
-		info->status_code = 126;
-		info->error_code = _CODE_EACCES;
-		error(info);
-		return;
-	}
-	if (status == 1)
-	{
-		execute(cmd, arguments, info, buff);
-		return;
-	}
-	if (current_directory(cmd, arguments, buff, info) == _TRUE)
-		return;
+    if (status == 1)
+    {
+        execute(cmd, arguments, info, buff);
+        return;
+    }
 
-	if (value_path != NULL)
-	{
-		execute(value_path, arguments, info, buff);
-		free_memory_p((void *)value_path);
-		return;
-		}
-/* Error handling for command not found */
-	info->status_code = 127;
-	info->error_code = _CODE_CMD_NOT_EXISTS;
-	error(info);
+    if (current_directory(cmd, arguments, buff, info) == _TRUE)
+        return;
+
+    info->value_path = which(cmd, info);
+    if (info->value_path != NULL)
+    {
+        execute(info->value_path, arguments, info, buff);
+        
+        /* Free dynamically allocated memory before returning */
+        free_memory_p((void *)info->value_path);
+        info->value_path = NULL; /* Set to NULL to avoid double freeing */
+    
+        return;
+    }
+
+    info->status_code = 127;
+    info->error_code = _CODE_CMD_NOT_EXISTS;
+    error(info);
+
+    /* Free dynamically allocated memory before returning */
+    free_memory_p((void *)info->value_path);
 }
